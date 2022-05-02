845dc9143ae66e34fe4729d5e1b4aa56fc9f8a8b
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-02 18:49:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
-resource "aws_iam_user" "lb" {
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
