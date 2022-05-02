05bd23a5521167ae94fe48b942e007eee2140c0b
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-02 18:16:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "redhat" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["redhat/images/hvm-ssd/ami-09d56f8956ab235b3*"]
   }
 
   filter {
